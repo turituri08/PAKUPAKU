b928e05ac357238053f6ba1398a96efdda1db8b0
turituri08 <t.y.bsk@icloud.com>
2021-07-06 05:33:32 +0000

    update users show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,12 @@ class User < ApplicationRecord
   has_many  :comments,     dependent: :destroy
   has_many  :likes,        dependent: :destroy
   has_many  :favorites,    dependent: :destroy
+  
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
