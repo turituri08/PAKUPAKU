c15be8b2fe1dd4ff53fa88606949c74418fbb0ff
turituri08 <t.y.bsk@icloud.com>
2021-07-07 06:16:51 +0000

    add sort function and contents_age pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get      '/contents/index_age0' => 'contents#index_age0', as: 'contents_age0'
+  get      '/contents/index_age1' => 'contents#index_age1', as: 'contents_age1'
+  get      '/contents/index_age2' => 'contents#index_age2', as: 'contents_age2'
+  get      '/contents/index_age3' => 'contents#index_age3', as: 'contents_age3'
   devise_for :users
   resources :contents,   only:[:new, :create, :index, :show, :update, :destroy] do
     resource :likes,     only:[:create, :destroy]
