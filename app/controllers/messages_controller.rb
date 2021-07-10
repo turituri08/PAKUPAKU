421f11c026bcde7ae2993a593b78bc98c3dff0c3
turituri08 <t.y.bsk@icloud.com>
2021-07-10 08:30:48 +0000

    add notification message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,10 +3,12 @@ class MessagesController < ApplicationController
 
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
+      message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
+      room    = message.room
+      message.create_notification_message(current_user, room, message)
       redirect_back(fallback_location: root_path)
     else
-      render room_path(@message.room_id), alert: "投稿に失敗しました"
+      render room_path(message.room_id), alert: "投稿に失敗しました"
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_034450) do
+ActiveRecord::Schema.define(version: 2021_07_10_072449) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2021_07_10_034450) do
     t.boolean "checked", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "room_id"
     t.index ["comment_id"], name: "index_notifications_on_comment_id"
     t.index ["content_id"], name: "index_notifications_on_content_id"
     t.index ["message_id"], name: "index_notifications_on_message_id"
