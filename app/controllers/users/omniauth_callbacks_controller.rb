9ab27f86da24a76673d7fd22f0040bd9e9497ed0
turituri08 <t.y.bsk@icloud.com>
2021-07-12 13:36:15 +0000

    failed google auth

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+   # callback for google
+  def google_oauth2
+    callback_for(:google)
+  end
+
+  # common callback method
+  def callback_for(provider)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    else
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+      redirect_to new_user_registration_url
+    end
+  end
+
+end
