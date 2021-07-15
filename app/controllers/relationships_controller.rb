75a72ce4d4107759b9661a012990a794ce67344c
turituri08 <t.y.bsk@icloud.com>
2021-07-15 06:09:12 +0000

    changes user_name font

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,22 +6,22 @@ class RelationshipsController < ApplicationController
     following = current_user.follow(@user)
     if following.save
       @user.create_notification_follow(current_user)
-      flash[:success] = 'ユーザーをフォローしました'
-      redirect_back(fallback_location: root_path)
-    else
-      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_back(fallback_location: root_path)
+      # flash[:success] = 'ユーザーをフォローしました'
+      # redirect_back(fallback_location: root_path)
+    # else
+    #   flash.now[:alert] = 'ユーザーのフォローに失敗しました'
+    #   redirect_back(fallback_location: root_path)
     end
   end
 
   def destroy
     following = current_user.unfollow(@user)
     if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_back(fallback_location: root_path)
-    else
-      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_back(fallback_location: root_path)
+      # flash[:success] = 'ユーザーのフォローを解除しました'
+      # redirect_back(fallback_location: root_path)
+    # else
+    #   flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
+    #   redirect_back(fallback_location: root_path)
     end
   end
 
